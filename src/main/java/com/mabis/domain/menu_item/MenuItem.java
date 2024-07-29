bc0463fcac19a6a6d8210dcd323e65b330e0f9fa
Guilherme D. Possas <possasg@gmail.com>
2024-07-29 17:49:16 -0300

    add: DishType and MenuItem objects maps

---
+++ src/main/java/com/mabis/domain/menu_item/MenuItem.java
@@ -0,0 +1,38 @@
+package com.mabis.domain.menu_item;
+
+import com.mabis.domain.dish_type.DishType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "menu_items")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuItem
+{
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column
+    private String url_image;
+
+    @Column
+    private String description;
+
+    @ManyToOne(targetEntity = DishType.class)
+    private DishType dish_type;
+}
