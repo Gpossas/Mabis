a85bcc95e5e87d8fff565b5a02d858bdff4c7cbf
Guilherme D. Possas <possasg@gmail.com>
2024-07-29 16:09:39 -0300

    add: initial commit

---
+++ src/main/java/com/mabis/MabisApplication.java
@@ -0,0 +1,13 @@
+package com.mabis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabis/MabisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
