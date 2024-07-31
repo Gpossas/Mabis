27d83295ccb156511f761434f49f73d193bb3c00
Guilherme D. Possas <possasg@gmail.com>
2024-07-31 17:24:29 -0300

    add: menu_items table

---
+++ src/main/resources/db/migration/V3__create_menu_item_table.sql
@@ -0,0 +1,13 @@
+create EXTENSION if not exists "pgcrypto";
+
+create table menu_items(
+    id UUID default gen_random_uuid() primary key,
+    name varchar(250) not null,
+    price numeric(6, 2) not null,
+    image_url text,
+    description text,
+    dish_type_id UUID,
+    constraint  fk_dish_type
+        foreign key (dish_type_id)
+        references dish_types (id)
+);
