c538bfc28b4015f44109563b8c1a851f19d949f3
Guilherme D. Possas <possasg@gmail.com>
2024-08-01 08:27:46 -0300

    add: category service

---
+++ src/main/java/com/mabis/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.mabis.services;
+
+import com.mabis.domain.category.Category;
+import com.mabis.domain.category.CreateCategoryDTO;
+import com.mabis.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService
+{
+    private final CategoryRepository category_repository;
+
+    public Category create_category(CreateCategoryDTO category_dto)
+    {
+        Category category = new Category();
+        category.setName(category_dto.name());
+        return category_repository.save(category);
+    }
+}
