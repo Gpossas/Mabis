771a67c5a2c8ba3ca49c9ea9476001773ca22e91
Guilherme D. Possas <possasg@gmail.com>
2024-08-01 17:09:07 -0300

    add: dish type repository

--- src/main/java/com/mabis/repositories/DishTypeRepository.java
+++ src/main/java/com/mabis/repositories/DishTypeRepository.java
@@ -1,10 +1,21 @@
 package com.mabis.repositories;
 
 import com.mabis.domain.dish_type.DishType;
+import com.mabis.domain.dish_type.ResponseDishTypeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface DishTypeRepository extends JpaRepository<DishType, UUID> {}
+public interface DishTypeRepository extends JpaRepository<DishType, UUID>
+{
+    @Query("""
+        SELECT new com.mabis.domain.dish_type.ResponseDishTypeDTO(d.id, d.name)
+        FROM DishType d
+        ORDER BY d.name
+    """)
+    List<ResponseDishTypeDTO> findAllDishTypesSorted();
+}
