54b568abbc18c9ecdb2920785cb9179cfce5ab8f
Guilherme D. Possas <possasg@gmail.com>
2024-08-01 16:27:04 -0300

    add: method findAllCategoriesSorted to CategoryRepository

--- src/main/java/com/mabis/repositories/CategoryRepository.java
+++ src/main/java/com/mabis/repositories/CategoryRepository.java
@@ -1,10 +1,21 @@
 package com.mabis.repositories;
 
 import com.mabis.domain.category.Category;
+import com.mabis.domain.category.ResponseCategoryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, UUID> {}
+public interface CategoryRepository extends JpaRepository<Category, UUID>
+{
+    @Query("""
+            SELECT new com.mabis.domain.category.ResponseCategoryDTO(c.id, c.name)
+            FROM Category c
+            ORDER BY c.name
+    """)
+    List<ResponseCategoryDTO> findAllCategoriesSorted();
+}
