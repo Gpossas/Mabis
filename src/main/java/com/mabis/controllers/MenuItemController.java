3cb86a5c293e5af97fe81a666c427ca2be0a70c5
Guilherme D. Possas <possasg@gmail.com>
2024-08-01 18:31:36 -0300

    add: add menu item controller

---
+++ src/main/java/com/mabis/controllers/MenuItemController.java
@@ -0,0 +1,26 @@
+package com.mabis.controllers;
+
+import com.mabis.domain.menu_item.CreateMenuItemDTO;
+import com.mabis.domain.menu_item.ResponseMenuItemDTO;
+import com.mabis.services.MenuItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu-items")
+@RequiredArgsConstructor
+public class MenuItemController
+{
+    private final MenuItemService menu_item_service;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseMenuItemDTO> create_menu_item(@RequestBody CreateMenuItemDTO menu_item_dto)
+    {
+        return new ResponseEntity<>(menu_item_service.create_menu_item(menu_item_dto), HttpStatus.CREATED);
+    }
+}
