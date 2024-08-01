d8114ef5682491f6378b71ffad81a30823e0809d
Guilherme D. Possas <possasg@gmail.com>
2024-08-01 18:27:18 -0300

    refact: changed response type to ResponseEntity

--- src/main/java/com/mabis/controllers/DishTypeController.java
+++ src/main/java/com/mabis/controllers/DishTypeController.java
@@ -4,6 +4,8 @@ import com.mabis.domain.dish_type.CreateDishTypeDTO;
 import com.mabis.domain.dish_type.ResponseDishTypeDTO;
 import com.mabis.services.DishTypeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +18,14 @@ public class DishTypeController
     private final DishTypeService dish_type_service;
 
     @PostMapping("/create")
-    public ResponseDishTypeDTO create_dish_type(@RequestBody CreateDishTypeDTO dish_type_dto)
+    public ResponseEntity<ResponseDishTypeDTO> create_dish_type(@RequestBody CreateDishTypeDTO dish_type_dto)
     {
-        return dish_type_service.create_dish_type(dish_type_dto);
+        return new ResponseEntity<>(dish_type_service.create_dish_type(dish_type_dto), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<ResponseDishTypeDTO> get_all_dish_types()
+    public ResponseEntity<List<ResponseDishTypeDTO>> get_all_dish_types()
     {
-        return dish_type_service.get_all();
+        return ResponseEntity.ok(dish_type_service.get_all());
     }
 }
