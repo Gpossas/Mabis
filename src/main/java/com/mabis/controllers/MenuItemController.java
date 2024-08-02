e3fae3d128c5eea91821f94ccfe7ce3a8e9c8eb7
Guilherme D. Possas <possasg@gmail.com>
2024-08-02 08:03:44 -0300

    add: validation to menu item

--- src/main/java/com/mabis/controllers/MenuItemController.java
+++ src/main/java/com/mabis/controllers/MenuItemController.java
@@ -3,6 +3,7 @@ package com.mabis.controllers;
 import com.mabis.domain.menu_item.CreateMenuItemDTO;
 import com.mabis.domain.menu_item.ResponseMenuItemDTO;
 import com.mabis.services.MenuItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class MenuItemController
     private final MenuItemService menu_item_service;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseMenuItemDTO> create_menu_item(@RequestBody CreateMenuItemDTO menu_item_dto)
+    public ResponseEntity<ResponseMenuItemDTO> create_menu_item(@Valid @RequestBody CreateMenuItemDTO menu_item_dto)
     {
         return new ResponseEntity<>(menu_item_service.create_menu_item(menu_item_dto), HttpStatus.CREATED);
     }
--- src/main/java/com/mabis/domain/menu_item/CreateMenuItemDTO.java
+++ src/main/java/com/mabis/domain/menu_item/CreateMenuItemDTO.java
@@ -1,13 +1,25 @@
 package com.mabis.domain.menu_item;
-
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 public record CreateMenuItemDTO(
+   @NotBlank()
    String name,
+
+   @NotNull
+   @Min(0)
+   @Max(9999)
    Float price,
+
    String description,
+
+   @NotNull
    UUID dish_type_id,
+
    MultipartFile image_url
 ) {}
