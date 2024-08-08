6032213ca48d2319f66d1c0361b3b460fd7b7d49
Guilherme D. Possas <possasg@gmail.com>
2024-08-08 09:49:27 -0300

    add: create menu item controller to handle multipart form data

--- src/main/java/com/mabis/controllers/MenuItemController.java
+++ src/main/java/com/mabis/controllers/MenuItemController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+
 
 @RestController
 @RequestMapping("/menu-items")
@@ -19,9 +18,10 @@ public class MenuItemController
 {
     private final MenuItemService menu_item_service;
 
-    @PostMapping("/create")
-    public ResponseEntity<ResponseMenuItemDTO> create_menu_item(@Valid @RequestBody CreateMenuItemDTO menu_item_dto)
-    {
+    @PostMapping(value = "/create", consumes = "multipart/form-data")
+    public ResponseEntity<ResponseMenuItemDTO> create_menu_item(
+            @Valid @ModelAttribute CreateMenuItemDTO menu_item_dto
+    ) throws IOException {
         return new ResponseEntity<>(menu_item_service.create_menu_item(menu_item_dto), HttpStatus.CREATED);
     }
 }
