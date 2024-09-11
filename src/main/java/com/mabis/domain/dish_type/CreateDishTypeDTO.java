2e34092890aa7d7e585b28ef6e7fe1f1ecf86f8d
Guilherme D. Possas <possasg@gmail.com>
2024-09-11 16:46:29 -0300

    add: decorator to limit length in dto

--- src/main/java/com/mabis/domain/dish_type/CreateDishTypeDTO.java
+++ src/main/java/com/mabis/domain/dish_type/CreateDishTypeDTO.java
@@ -1,5 +1,6 @@
 package com.mabis.domain.dish_type;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 
-public record CreateDishTypeDTO(@NotBlank String name) {}
+public record CreateDishTypeDTO(@NotBlank @Max(30) String name) {}
