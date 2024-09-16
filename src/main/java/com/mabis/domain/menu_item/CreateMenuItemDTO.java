56e09258ced49b4796183e8c475ad2ea41153ab8
Guilherme D. Possas <possasg@gmail.com>
2024-09-16 07:54:47 -0300

    fix: validator of number in string

--- src/main/java/com/mabis/domain/menu_item/CreateMenuItemDTO.java
+++ src/main/java/com/mabis/domain/menu_item/CreateMenuItemDTO.java
@@ -1,15 +1,12 @@
 package com.mabis.domain.menu_item;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 public record CreateMenuItemDTO(
    @NotBlank()
-   @Max(250)
+   @Size(max = 250)
    String name,
 
    @NotNull
