1d91824c7277a101da2ed2936475f4d8001c7454
Guilherme D. Possas <possasg@gmail.com>
2024-09-21 17:54:19 -0300

    add: test for upload when image is not passed

--- src/test/java/com/mabis/services/MenuItemServiceTest.java
+++ src/test/java/com/mabis/services/MenuItemServiceTest.java
@@ -1,5 +1,7 @@
 package com.mabis.services;
 
+import com.mabis.domain.attachment.AttachmentService;
+import com.mabis.domain.attachment.StorageServiceFactory;
 import com.mabis.domain.dish_type.DishType;
 import com.mabis.domain.menu_item.CreateMenuItemDTO;
 import com.mabis.domain.menu_item.MenuItem;
@@ -11,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -24,6 +28,12 @@ class MenuItemServiceTest
 
     @Mock
     private DishTypeService dish_type_service;
+
+    @Mock
+    private StorageServiceFactory storage_service_factory;
+
+    @Mock
+    private ApplicationContext context;
 
     @InjectMocks
     private MenuItemService menu_item_service;
@@ -46,5 +56,17 @@ class MenuItemServiceTest
 
         assertEquals("Risotto", result.name());
         assertEquals(123, result.price());
+    }
+
+    @Test
+    void test_upload_when_image_is_null()
+    {
+        AttachmentService attachment_service = Mockito.mock(AttachmentService.class);
+        MenuItem menu_item = Mockito.mock(MenuItem.class);
+
+        Mockito.verify(storage_service_factory, Mockito.never()).get_service(Mockito.any());
+        Mockito.verify(context, Mockito.never()).getBean(AttachmentService.class);
+        Mockito.verify(attachment_service, Mockito.never()).upload(Mockito.any());
+        Mockito.verify(menu_item, Mockito.never()).setImage_url(Mockito.any());
     }
 }
