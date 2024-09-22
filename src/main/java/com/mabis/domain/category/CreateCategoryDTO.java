26056ab6dc65bafc08d7b7bfcb332f00e0a0cbb4
Guilherme D. Possas <possasg@gmail.com>
2024-09-22 15:11:20 -0300

    fix: wrong validator to limit string size

--- src/main/java/com/mabis/domain/category/CreateCategoryDTO.java
+++ src/main/java/com/mabis/domain/category/CreateCategoryDTO.java
@@ -2,5 +2,6 @@ package com.mabis.domain.category;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CreateCategoryDTO(@NotBlank @Max(30) String name) {}
+public record CreateCategoryDTO(@NotBlank @Size(max = 30) String name) {}
