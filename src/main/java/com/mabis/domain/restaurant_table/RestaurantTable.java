5fbfccd1dd5ee7ec5f8c2e907de78d947106f78b
Guilherme D. Possas <possasg@gmail.com>
2024-10-03 20:12:02 -0300

    add: table domain

---
+++ src/main/java/com/mabis/domain/restaurant_table/RestaurantTable.java
@@ -0,0 +1,39 @@
+package com.mabis.domain.restaurant_table;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "tables")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class RestaurantTable
+{
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, name = "table_number")
+    private int number;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column
+    private String qr_code_url;
+
+    @Column(nullable = false)
+    private String status = "inactive";
+
+    public RestaurantTable(int number, int quantity)
+    {
+        this.number = number;
+        this.capacity = quantity;
+    }
+}
