d41e0003fd397fa60d3c480d1a076222eeed761f
Guilherme D. Possas <possasg@gmail.com>
2024-10-03 20:05:46 -0300

    add: table service

---
+++ src/main/java/com/mabis/services/TableService.java
@@ -0,0 +1,30 @@
+package com.mabis.services;
+
+import com.mabis.domain.restaurant_table.CreateTablesDTO;
+import com.mabis.domain.restaurant_table.RestaurantTable;
+import com.mabis.repositories.TableRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TableService
+{
+    private final TableRepository table_repository;
+
+    public void create_tables(CreateTablesDTO dto)
+    {
+        Optional<Integer> max_table_number = table_repository.get_max_table_number();
+
+        ArrayList<RestaurantTable> tables = new ArrayList<>();
+        for (int table_number = max_table_number.orElse(0) + 1, i = 0; i < dto.tables_quantity(); table_number++, i++)
+        {
+            tables.add(new RestaurantTable(table_number, dto.capacity()));
+        }
+
+        table_repository.saveAll(tables);
+    }
+}
