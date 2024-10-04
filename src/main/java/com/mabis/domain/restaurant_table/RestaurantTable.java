bd25680d3049bdde9ab58e59c2a0f1378a9455f8
Guilherme D. Possas <possasg@gmail.com>
2024-10-04 06:37:17 -0300

    fix: Table entity without NoArgsConstructor

--- src/main/java/com/mabis/domain/restaurant_table/RestaurantTable.java
+++ src/main/java/com/mabis/domain/restaurant_table/RestaurantTable.java
@@ -4,6 +4,7 @@ package com.mabis.domain.restaurant_table;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RestaurantTable
 {
     @Id
