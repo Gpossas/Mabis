156f9193fbf7e2d4149130e566ce784f25c2e5bc
Guilherme D. Possas <possasg@gmail.com>
2024-10-04 19:37:45 -0300

    add: QR Code service

---
+++ src/main/java/com/mabis/services/QRCodeService.java
@@ -0,0 +1,23 @@
+package com.mabis.services;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class QRCodeService
+{
+    public BufferedImage generate_qr_code_image(String barcodeText) throws WriterException
+    {
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix =
+                barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+}
