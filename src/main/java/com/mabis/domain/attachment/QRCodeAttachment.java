8ed1efe6d9649f4221f7253546c06ce0427581af
Guilherme D. Possas <possasg@gmail.com>
2024-10-05 18:13:18 -0300

    add: qr code attachment

---
+++ src/main/java/com/mabis/domain/attachment/QRCodeAttachment.java
@@ -0,0 +1,32 @@
+package com.mabis.domain.attachment;
+
+import java.util.UUID;
+
+public class QRCodeAttachment implements Attachment
+{
+    private final byte[] byte_array;
+
+    @Override
+    public String get_name()
+    {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public String get_content_type()
+    {
+        return "PNG";
+    }
+
+    @Override
+    public byte[] get_bytes()
+    {
+        return byte_array;
+    }
+
+    public QRCodeAttachment(byte[] byte_array)
+    {
+        this.byte_array = byte_array;
+    }
+
+}
