81c1c11ff41b67f18c50195ccbeee96f336da412
Guilherme D. Possas <possasg@gmail.com>
2024-10-07 13:28:54 -0300

    remove: QR Code exceptions from method signature

--- src/main/java/com/mabis/services/QRCodeService.java
+++ src/main/java/com/mabis/services/QRCodeService.java
@@ -5,6 +5,8 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.mabis.exceptions.QRCodeEncodeException;
+import com.mabis.exceptions.QRCodeToBytesException;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -13,13 +15,27 @@ import java.io.IOException;
 @Service
 public class QRCodeService
 {
-    public byte[] generate_qr_code(String barcodeText) throws WriterException, IOException {
+    public byte[] generate_qr_code(String barcodeText)
+    {
         QRCodeWriter barcodeWriter = new QRCodeWriter();
-        BitMatrix bit_matrix =
-                barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
-
         ByteArrayOutputStream byte_array = new ByteArrayOutputStream();
-        MatrixToImageWriter.writeToStream(bit_matrix, "PNG", byte_array);
+
+        try
+        {
+            BitMatrix bit_matrix =
+                    barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+
+            MatrixToImageWriter.writeToStream(bit_matrix, "PNG", byte_array);
+        }
+        catch (WriterException e)
+        {
+            throw new QRCodeEncodeException();
+        }
+        catch (IOException e)
+        {
+            throw new QRCodeToBytesException();
+        }
+
         return byte_array.toByteArray();
     }
 }
