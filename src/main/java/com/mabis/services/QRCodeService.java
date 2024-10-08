fbed1cf46c0e97de64e5826e65e9d5c6d47bf3e4
Guilherme D. Possas <possasg@gmail.com>
2024-10-08 16:28:45 -0300

    refact: concrete implementation of zxing for decoupled implementation using qr code generator interface

--- src/main/java/com/mabis/services/QRCodeService.java
+++ src/main/java/com/mabis/services/QRCodeService.java
@@ -1,41 +1,18 @@
 package com.mabis.services;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.mabis.exceptions.QRCodeEncodeException;
-import com.mabis.exceptions.QRCodeToBytesException;
+import com.mabis.domain.qr_code.QRCodeGenerator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class QRCodeService
 {
-    public byte[] generate_qr_code(String barcodeText)
+    private final QRCodeGenerator qr_code_generator;
+
+    public byte[] generate_qr_code(String content)
     {
-        QRCodeWriter barcodeWriter = new QRCodeWriter();
-        ByteArrayOutputStream byte_array = new ByteArrayOutputStream();
-
-        try
-        {
-            BitMatrix bit_matrix =
-                    barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
-
-            MatrixToImageWriter.writeToStream(bit_matrix, "PNG", byte_array);
-        }
-        catch (WriterException e)
-        {
-            throw new QRCodeEncodeException();
-        }
-        catch (IOException e)
-        {
-            throw new QRCodeToBytesException();
-        }
-
-        return byte_array.toByteArray();
+        return this.qr_code_generator.generate_qr_code(content);
     }
 }
