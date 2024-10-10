58b20f009a9bd15f5ddb08e1e97bea3ea283a0a7
Guilherme D. Possas <possasg@gmail.com>
2024-10-10 18:48:38 -0300

    add: AttachmentRepository

---
+++ src/main/java/com/mabis/repositories/AttachmentRepository.java
@@ -0,0 +1,10 @@
+package com.mabis.repositories;
+
+import com.mabis.domain.attachment.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, UUID> {}
