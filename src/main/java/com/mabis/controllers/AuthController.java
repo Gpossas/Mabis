de6ca326e0c9a02fb437d2abc5409c15bc5c1c4b
Guilherme D. Possas <possasg@gmail.com>
2024-10-20 16:34:04 -0300

    add: login endpoint

--- src/main/java/com/mabis/controllers/AuthController.java
+++ src/main/java/com/mabis/controllers/AuthController.java
@@ -1,15 +1,19 @@
 package com.mabis.controllers;
 
+import com.mabis.domain.user.LoginRequestDTO;
 import com.mabis.domain.user.LoginResponseDTO;
 import com.mabis.domain.user.RegisterUserDTO;
 import com.mabis.domain.user.User;
+import com.mabis.exceptions.UnmatchPassword;
 import com.mabis.exceptions.UserEmailAlreadyInUse;
 import com.mabis.repositories.UserRepository;
 import com.mabis.services.JWTService;
+import com.mabis.services.UserDetailsServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +28,7 @@ public class AuthController
     private final PasswordEncoder password_encoder;
     private final UserRepository user_repository;
     private final JWTService jwt_service;
+    private final UserDetailsServiceImpl user_details_service;
 
     @PostMapping("/register")
     public ResponseEntity<LoginResponseDTO> register(@Valid @RequestBody RegisterUserDTO dto)
@@ -41,6 +46,20 @@ public class AuthController
         String token = jwt_service.generate_token(user);
 
         return new ResponseEntity<>(LoginResponseDTO.from_user(user, token), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(LoginRequestDTO credentials)
+    {
+        UserDetails user = user_details_service.loadUserByUsername(credentials.email());
+        if (!password_encoder.matches(user.getPassword(), credentials.password()))
+        {
+            throw new UnmatchPassword();
+        }
+
+        String token = jwt_service.generate_token((User) user);
+
+        return ResponseEntity.ok(LoginResponseDTO.from_user((User) user, token));
     }
 }
 
