08836679eb17c4e60fb7763e31588c1ceb119b30
Guilherme D. Possas <possasg@gmail.com>
2024-10-20 15:39:17 -0300

    add: user entity

---
+++ src/main/java/com/mabis/domain/user/User.java
@@ -0,0 +1,54 @@
+package com.mabis.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User
+{
+    @Getter
+    @AllArgsConstructor
+    enum Roles
+    {
+        OWNER("OWNER"),
+        WAITER("WAITER"),
+        COOK("COOK");
+        private final String role;
+    }
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    private String last_name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Roles role;
+
+    public User(RegisterUserDTO dto)
+    {
+        this.first_name = dto.first_name();
+        this.last_name = dto.last_name();
+        this.email = dto.email();
+        this.role = dto.role();
+    }
+}
