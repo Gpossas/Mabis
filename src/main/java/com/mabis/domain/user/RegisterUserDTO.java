dc58dad2ba3e8d1cf3a7e2e32df8064a6cfb4192
Guilherme D. Possas <possasg@gmail.com>
2024-10-21 09:09:03 -0300

    remove: unused import

--- src/main/java/com/mabis/domain/user/RegisterUserDTO.java
+++ src/main/java/com/mabis/domain/user/RegisterUserDTO.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-import javax.management.relation.Role;
 
 public record RegisterUserDTO(
         @NotBlank
