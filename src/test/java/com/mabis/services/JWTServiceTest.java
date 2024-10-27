a0435e5759ffe76aa91295e61f2065bd1c447834
Guilherme D. Possas <possasg@gmail.com>
2024-10-27 06:20:08 -0300

    fix: unit test that verify secret not set without mocks in JWTServiceTest

--- src/test/java/com/mabis/services/JWTServiceTest.java
+++ src/test/java/com/mabis/services/JWTServiceTest.java
@@ -1,35 +1,39 @@
 package com.mabis.services;
 
-import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.mabis.domain.user.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
 class JWTServiceTest
 {
-
+    @InjectMocks
+    JWTService jwt_service;
 
     @Test
     void test_generate_token_throw_error_if_secret_not_set()
     {
-        Algorithm algorithm = Algorithm.HMAC256("");
+        String invalidSecret = "";
 
-        assertThatThrownBy(() -> JWT.create()
-                .withIssuer("")
-                .withSubject("")
-                .withExpiresAt(Mockito.mock(Date.class))
-                .sign(algorithm)
-        ).isInstanceOf(IllegalArgumentException.class)
-         .hasMessage("Empty key");
+        // Mock the static method Algorithm.HMAC256
+        try (MockedStatic<Algorithm> mockedAlgorithm = mockStatic(Algorithm.class)) {
+            // Define the behavior when HMAC256 is called with an empty secret
+            mockedAlgorithm.when(() -> Algorithm.HMAC256(invalidSecret))
+                    .thenThrow(new IllegalArgumentException());
+
+            // Now, call the method that generates the token and verify the exception is thrown
+            assertThatThrownBy(() -> jwt_service.generate_token(Mockito.mock(User.class)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("The Algorithm cannot be null.");
+        }
     }
 
     @Test
