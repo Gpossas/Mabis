203caa131d108f6265b227ced42e98ed713ac201
Guilherme D. Possas <possasg@gmail.com>
2024-10-29 11:33:28 -0300

    add: add unit test in TableServiceTest to verify tables number normalization

--- src/test/java/com/mabis/services/TableServiceTest.java
+++ src/test/java/com/mabis/services/TableServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,5 +40,21 @@ class TableServiceTest
 
         assertEquals(EXPECTED_TABLES_CREATED_COUNT, tables_list_captor.getValue().size());
         assertEquals(EXPECTED_LAST_TABLE_NUMBER, tables_list_captor.getValue().removeLast().getNumber());
+    }
+
+    @Test
+    void test_tables_normalization()
+    {
+        RestaurantTable table1 = new RestaurantTable(3,1);
+        RestaurantTable table2 = new RestaurantTable(10,1);
+        RestaurantTable table3 = new RestaurantTable(17,1);
+
+        Mockito.when(table_repository.findAll()).thenReturn(List.of(table1, table2, table3));
+
+        table_service.normalize_table_numbers_sequence();
+
+        Mockito.verify(table_repository).saveAll(tables_list_captor.capture());
+
+        assertIterableEquals(List.of(1, 2, 3), tables_list_captor.getValue().stream().map(RestaurantTable::getNumber).toList());
     }
 }
