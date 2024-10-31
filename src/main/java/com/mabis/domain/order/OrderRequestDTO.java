3da16694cd0d056f0e1679e0925128ce007e8c1f
Guilherme D. Possas <possasg@gmail.com>
2024-10-31 14:55:48 -0300

    add: table token in OrderRequestDTO to verify credentials

--- src/main/java/com/mabis/domain/order/OrderRequestDTO.java
+++ src/main/java/com/mabis/domain/order/OrderRequestDTO.java
@@ -6,4 +6,4 @@ import java.util.Set;
 import java.util.UUID;
 
 
-public record OrderRequestDTO(@NotNull UUID table_id, Set<OrderItemDTO> menu_items) {}
+public record OrderRequestDTO(@NotNull UUID table_id, @NotNull String table_token, @NotNull Set<OrderItemDTO> menu_items) {}
