6165ea56c16dd56019130025e06253e1f1f2d350
Guilherme D. Possas <possasg@gmail.com>
2024-10-31 10:07:17 -0300

    test: add name of methods to test in OrderServiceTest

---
+++ src/test/java/com/mabis/services/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package com.mabis.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest
+{
+    @Test
+    void test_place_order_non_existent_table_throw_exception(){}
+
+    @Test
+    void test_place_order_non_existent_menu_item_throw_exception(){}
+
+    @Test
+    void test_place_order_not_active_table_throw_exception(){}
+
+    @Test
+    void test_successful_place_order(){}
+
+    @Test
+    void test_successful_delete_order(){}
+}
