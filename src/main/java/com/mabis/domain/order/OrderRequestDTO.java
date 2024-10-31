0a3b124542c6077c89325638a086f24a11814db0
Guilherme D. Possas <possasg@gmail.com>
2024-10-31 17:27:21 -0300

    fix: updated OrderRequestDTO to receive list instead of set

--- src/main/java/com/mabis/domain/order/OrderRequestDTO.java
+++ src/main/java/com/mabis/domain/order/OrderRequestDTO.java
@@ -2,8 +2,8 @@ package com.mabis.domain.order;
 
 import jakarta.validation.constraints.NotNull;
 
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
 
-public record OrderRequestDTO(@NotNull UUID table_id, @NotNull String table_token, @NotNull Set<OrderItemDTO> menu_items) {}
+public record OrderRequestDTO(@NotNull UUID table_id, @NotNull String table_token, @NotNull List<OrderItemDTO> menu_items) {}
