cbc1ebeb4b584998d0b1d924f3ca88fba32d11a8
Guilherme D. Possas <possasg@gmail.com>
2024-10-31 13:48:08 -0300

    add: assert of exceptions in OrderServiceTest

--- src/test/java/com/mabis/services/OrderServiceTest.java
+++ src/test/java/com/mabis/services/OrderServiceTest.java
@@ -1,26 +1,57 @@
 package com.mabis.services;
 
+import com.mabis.exceptions.MenuItemNotFound;
+import com.mabis.exceptions.NotActiveTableException;
+import com.mabis.exceptions.TableNotFoundException;
+import com.mabis.exceptions.TableTokenNotMatchException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest
 {
     @Test
-    void test_place_order_non_existent_table_throw_exception(){}
+    void test_place_order_non_existent_table_throw_exception()
+    {
+        assertThatThrownBy(() -> order_service.place_order())
+                .isInstanceOf(TableNotFoundException.class)
+                .hasMessage("Table not found");
+    }
 
     @Test
-    void test_place_order_non_existent_menu_item_throw_exception(){}
+    void test_place_order_non_existent_menu_item_throw_exception()
+    {
+        assertThatThrownBy(() -> order_service.place_order())
+                .isInstanceOf(MenuItemNotFound.class)
+                .hasMessage("Table not found");
+    }
 
     @Test
-    void test_place_order_not_active_table_throw_exception(){}
+    void test_place_order_not_active_table_throw_exception()
+    {
+        assertThatThrownBy(() -> order_service.place_order())
+                .isInstanceOf(NotActiveTableException.class)
+                .hasMessage("Table is not active");
+    }
 
     @Test
-    void test_delete_non_existent_order_throw_exception(){}
+    void test_delete_non_existent_order_throw_exception()
+    {
+        assertThatThrownBy(() -> order_service.delete_order())
+                .isInstanceOf(NotActiveTableException.class)
+                .hasMessage("Table is not active");
+    }
 
     @Test
-    void test_credentials_not_match_to_place_order_throw_exception(){}
+    void test_credentials_not_match_to_place_order_throw_exception()
+    {
+        assertThatThrownBy(() -> order_service.delete_order())
+                .isInstanceOf(TableTokenNotMatchException.class)
+                .hasMessage("You're not authorized to place orders in table " + table_number);
+    }
 
     @Test
     void test_successful_place_order(){}
