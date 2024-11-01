b484236111ec7bf960a70afb50c71448a540b63d
Guilherme D. Possas <possasg@gmail.com>
2024-11-01 08:26:38 -0300

    add: finished place_order in OrderService

--- src/main/java/com/mabis/services/OrderService.java
+++ src/main/java/com/mabis/services/OrderService.java
@@ -1,13 +1,20 @@
 package com.mabis.services;
 
+import com.mabis.domain.menu_item.MenuItem;
+import com.mabis.domain.order.Order;
+import com.mabis.domain.order.OrderItemDTO;
 import com.mabis.domain.order.OrderRequestDTO;
 import com.mabis.domain.restaurant_table.RestaurantTable;
 import com.mabis.exceptions.NotActiveTableException;
 import com.mabis.exceptions.TableTokenNotMatchException;
+import com.mabis.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -15,6 +22,8 @@ import org.springframework.stereotype.Service;
 public class OrderService
 {
     private final TableService table_service;
+    private final MenuItemService menu_item_service;
+    private final OrderRepository order_repository;
 
     public void place_order(OrderRequestDTO dto)
     {
@@ -22,6 +31,14 @@ public class OrderService
         this.verify_table_status_active(table);
         this.verify_credentials_authorized(table, dto.table_token());
 
+        List<Order> orders = new ArrayList<>();
+        for (OrderItemDTO order_item: dto.menu_items())
+        {
+            MenuItem menu_item = menu_item_service.get_by_id(order_item.menu_item_id());
+            orders.add(new Order(order_item.quantity(), menu_item.getPrice(), order_item.description(), table, menu_item));
+        }
+
+        order_repository.saveAll(orders);
     }
 
     private void verify_table_status_active(RestaurantTable table)
